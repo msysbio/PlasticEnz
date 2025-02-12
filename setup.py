931d9eb20bfa89938ab44a5693bc3a8901811a03
Anna Maria K <82043913+AMK06-1993@users.noreply.github.com>
2025-02-12 11:10:38 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,24 @@ setup(
     version="0.1.0",
     description="A toolset for identifying plastic-degrading enzymes",
     author="Your Name",
+    include_package_data=True,
     packages=find_packages(),
-    package_data={'PlasticEnz' : ['test/*']},
     python_requires=">=3.11.11",
     install_requires=required_packages,
+    package_data={
+        'PlasticEnz': [
+            'data/*',
+            'data/**/*',
+            'test/*',
+            'test/**/*',
+        ]
+    },
     entry_points={
         "console_scripts": [
             "plasticenz=PlasticEnz.main:main",
         ],
     },
 )
-
 
 
 
